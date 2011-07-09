9c39a48b792980cd31acb6ab31c5c65c24cf8b18
Jamie Wright <jamie@brilliantfantastic.com>
2011-07-09 00:53:47 -0400

    Copy text styles for moved element. Move element when the user stops typing. Added destination parameter.

--- jQuery.fantastical.js
+++ jQuery.fantastical.js
@@ -1,36 +1,79 @@
 (function( $ ) {
-  $.fn.fantastical = function() {
+  $.fn.fantastical = function( destination, options ) {
     var pattern = /(\w+)[.!?]?\s*$/;
+    var character_offset = 10;	// TODO: Calculate the width of a character based on the style of the temp width
+	var idleTimer = null;
+	var moved = false;
+
+    var settings = {
+		'idleWaitTime'	: 2000
+	};
     
     return this.each(function() {
       var $this = $(this);
-      
-      $this.keyup(function(event) {
-        if (event.keyCode == '32') {
-          var match = pattern.exec($this.val());
+
+	  if ( options ) { 
+		$.extend( settings, options );
+	  }
+
+	  function idle() {
+		move();
+	  }
+	
+	  function move() {
+		if (!moved) {
+		  var match = pattern.exec($this.val());
           if (match[0]) {
             // move text from source to destination
-            $this.parent().append("<span id='temp'>" + match[0] + "</span>");
-            var temp = $this.parent().children("#temp");
-            
-            // TODO: Calculate the width of a character based on the style of the temp width
-            var offset = { top: $this.offset().top, left: ($this.offset().left + ($this.caret() * 10)) };
+            $this.parent().append("<span id='__temp'>" + match[0] + "</span>");
+            var temp = $this.parent().children("#__temp");
+  		    // copy the css from the source
+	  	    temp.css("font-size", $this.css("font-size"));
+		    temp.css("font-weight", $this.css("font-weight"));
+		    temp.css("font-family", $this.css("font-family"));
+		    temp.css("color", $this.css("color"));
+
+		    var left = $this.offset().left + ($this.caret() * character_offset);
+		    if ((($this.caret() * character_offset) + (match[0].length * character_offset)) > $this.width()) {
+			  left = ($this.offset().left + $this.width()) - (match[0].length * character_offset);
+		    }
+            var offset = { top: $this.offset().top, left: left };
             temp.offset( offset );
             var from = temp.offset();
-            
-            var to = $("#destination .inner").offset();
-            var width = $("#destination .inner span").width();
-            
-            // TODO: The + or the - depends on the relative positioning of from and to
+
+			// The destination must have some content if it is an inline element to get the offset
+			if (destination.html().length == 0) {
+				destination.html(".");
+			}
+            var to = destination.offset();
+            var width = destination.width();
+
             var top = "+=" + (to.top - from.top) + "px";
-            var left = "+=" + ((to.left + width) - from.left) + "px";
-            
+            left = "+=" + ((to.left + width) - from.left) + "px";
+
             temp.animate({top: top, left: left}, function() {
-              $("#destination .inner span").html($this.val());
+              destination.html($this.val());
               temp.remove();
+              moved = true;
             });
           }
-        }
+		}
+	  }
+      
+      $this.keyup(function(event) {
+		if (!destination) { return; }
+		
+		if (idleTimer) {
+			clearTimeout(idleTimer);
+		}
+		
+        if (event.keyCode == '32') {
+		  move();
+        } else {
+	        moved = false;
+			idleTimer = setTimeout(idle, settings.idleWaitTime);
+		}
+
       });
 	});
   };
